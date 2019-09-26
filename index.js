62aaea09d35800e2daef9dffc1584c1b9cf1f701
Jacob H. Haven <jacob@jhaven.me>
2019-09-26 08:38:44 -0700

    Basic JS regexp matching of homograph names

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const punycode = require('punycode');
-const fs = require('fs');
-const confusables = require('./confusables.json');
+const homoglyphs = require('./homoglyphs.json');
 const tlds = require('./tlds_ascii.json');
 
-let totalCount = 0;
-const tldHomographs = {};
-for (const tld of tlds {
-    const unicodeDomain = punycode.toUnicode(tld); // For IDN TLDs
-    let homographs = [''] // Array of Unicode strings
-    for (const char of unicodeDomain) {
-        const homoglyphSet = new Set([char]);
-        confusables[char].forEach(({ c }) => homoglyphSet.add(c));
-
-        const newHomographs = new Array(homographs.length * homoglyphSet.size);
-        Array.from(homoglyphSet).forEach((homoglyph, i) => {
-            homographs.forEach((partial, j) => {
-                newHomographs[j * homoglyphSet.size + i] = partial + homoglyph;
-            })
-        })
-        homographs = Array.from(new Set(newHomographs));
+const matchStrs = []
+for (const tld of tlds) {
+    const unicode = punycode.toUnicode(tld);
+    let matchStr = ''
+    for (const char of unicode) {
+        const matches = homoglyphs[char] ? homoglyphs[char].join("|") : char
+        matchStr += `(${matches})`
     }
-    console.log(tld, homographs.length)
-    totalCount += homographs.length
-    fs.writeFile(
-        `homographs/${tld}.json`,
-        JSON.stringify(homographs, null, 2),
-        'utf8',
-        err => { if (err) console.log(err) });
+    matchStrs.push(`(${matchStr})`)
 }
-console.log(totalCount)
+
+const homographRegexp = new RegExp(`^${matchStrs.join('|')}$`, 'u')
+
+function isTldHomograph(name) {
+    return homographRegexp.test(punycode.toUnicode(name))
+}
+
+const testHomograph = name => {
+    console.log(name, isTldHomograph(name))
+}
+
+testHomograph('')
+testHomograph('asdfkasldflaksjdflaksd')
+testHomograph('xn--kpu38a')
+testHomograph('con')
+testHomograph('corn')
+testHomograph('xn-o-eka')
